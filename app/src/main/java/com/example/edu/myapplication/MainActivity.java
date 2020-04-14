593263470638712677c7662b8c193fb7349c536c
MengDieLou <1912148105@qq.com>
2020-04-14 22:16:32 +0800

    添加了启动图片，让app花哨了许多

--- app/src/main/java/com/example/edu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/edu/myapplication/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.edu.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
     }
 }
